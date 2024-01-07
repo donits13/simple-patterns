e993c1070e29cd025977ed0eeeaf58d751ab1825
donits13 <donitaroseriparip@gmail.com>
2024-01-07 21:31:32 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        if(i == 1 && j == 1 || 
+            //           i == 1 && j == 2 || 
+            //           i == 1 && j == 3 ||
+            //           i == 2 && j == 1 ||
+            //           i == 2 && j == 2 ||
+            //           i == 2 && j == 3 ||
+            //           i == 3 && j == 1 ||
+            //           i == 3 && j == 2 ||
+            //           i == 3 && j == 3)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("*");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 5 ; i++)
+            //{
+            //    for (int j = 0; j < 5 ; j++)
+            //    {
+            //        if (i == 0 && j == 1 ||
+            //        i == 0 && j == 2 ||
+            //        i == 0 && j == 3 ||
+            //        i == 0 && j == 4 ||
+            //        i == 1 && j == 2 ||
+            //        i == 1 && j == 3 ||
+            //        i == 1 && j == 4 ||
+            //        i == 2 && j == 3 ||
+            //        i == 2 && j == 4 ||
+            //        i == 3 && j == 4)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("*");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        if (i == 1 && j == 4 ||
+            //        i == 2 && j == 3 ||
+            //        i == 2 && j == 4 ||
+            //        i == 3 && j == 2 ||
+            //        i == 3 && j == 3 ||
+            //        i == 3 && j == 4 ||
+            //        i == 4 && j == 1 ||
+            //        i == 4 && j == 2 ||
+            //        i == 4 && j == 3 ||
+            //        i == 4 && j == 4)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("*");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+        }
+    }
+}
